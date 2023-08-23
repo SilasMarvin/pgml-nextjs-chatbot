b4b672089711aad44937884ade9a46ea2af2b227
SilasMarvin <19626586+SilasMarvin@users.noreply.github.com>
2023-08-23 14:59:06 -0700

    Debugging

--- next.config.js
+++ next.config.js
@@ -1,7 +1,116 @@
 /** @type {import('next').NextConfig} */
 
 // const path = require("path");
-const os = require("os");
+// const os = require("os");
+
+// {
+//   target: "node",
+//   output: {
+//     libraryTarget: "commonjs2"
+//   },
+//   module: {
+//     rules: [
+//       {
+//         // For node binary relocations, include ".node" files as well here
+//         test: /\.(m?js|node)$/,
+//         // it is recommended for Node builds to turn off AMD support
+//         parser: { amd: false },
+//         use: {
+//           loader: '@vercel/webpack-asset-relocator-loader',
+//           options: {
+//             // optional, base folder for asset emission (eg assets/name.ext)
+//             outputAssetBase: 'assets',
+//             // optional, restrict asset emissions to only the given folder.
+//             filterAssetBase: process.cwd(),
+//             // optional, permit entire __dirname emission
+//             // eg `const nonAnalyzable = __dirname` can emit everything in the folder
+//             emitDirnameAll: false,
+//             // optional, permit entire filterAssetBase emission
+//             // eg `const nonAnalyzable = process.cwd()` can emit everything in the cwd()
+//             emitFilterAssetBaseAll: false,
+//             // optional, custom functional asset emitter
+//             // takes an asset path and returns the replacement
+//             // or returns false to skip emission
+//             customEmit: (path, { id, isRequire }) => false | '"./custom-replacement"',
+//             // optional, a list of asset names already emitted or
+//             // defined that should not be emitted
+//             existingAssetNames: [],
+//             wrapperCompatibility: false, // optional, default
+//             // build for process.env.NODE_ENV = 'production'
+//             production: true, // optional, default is undefined
+//             cwd: process.cwd(), // optional, default
+//             debugLog: false, // optional, default
+//           }
+//         }
+//       }
+//     ]
+//   }
+// }
+
+// const path = require("path");
+//
+// const nextConfig = {
+//   webpack: (config, { dev, isServer, webpack, nextRuntime }) => {
+//     config.module.rules.push({
+//       test: /\.node$/,
+//       loader: "node-loader",
+//       options: {
+//         name: "[path][name].[ext]",
+//         flags: os.constants.dlopen.RTLD_NOW,
+//       },
+//     });
+//     return config;
+//   },
+// };
+
+// const nextConfig = {
+//   webpack: (config, { dev, isServer, webpack, nextRuntime }) => {
+//     config.node = {
+//       global: true,
+//       __filename: true,
+//       __dirname: true,
+//     };
+
+//     config.module.rules.push({
+//       test: /\.(node)$/,
+//       parser: { amd: false },
+//       use: {
+//         loader: "@vercel/webpack-asset-relocator-loader",
+//         options: {
+//           // optional, base folder for asset emission (eg assets/name.ext)
+//           // outputAssetBase: "chunks",
+//           // optional, restrict asset emissions to only the given folder.
+//           filterAssetBase: process.cwd(),
+//           // optional, permit entire __dirname emission
+//           // eg `const nonAnalyzable = __dirname` can emit everything in the folder
+//           // emitDirnameAll: true,
+//           // optional, permit entire filterAssetBase emission
+//           // eg `const nonAnalyzable = process.cwd()` can emit everything in the cwd()
+//           // emitFilterAssetBaseAll: false,
+//           // // optional, custom functional asset emitter
+//           // // takes an asset path and returns the replacement
+//           // // or returns false to skip emission
+//           // customEmit: (path, { id, isRequire }) =>
+//           customEmit: (path, { id, isRequire }) => {
+//             console.log("\nWHAT THE NUTS\n");
+//             return "./test"
+//           },
+//           //   false | '"./custom-replacement"',
+//           // // optional, a list of asset names already emitted or
+//           // // defined that should not be emitted
+//           // existingAssetNames: [],
+//           // wrapperCompatibility: false, // optional, default
+//           // // build for process.env.NODE_ENV = 'production'
+//           production: true, // optional, default is undefined
+//           // cwd: process.cwd(), // optional, default
+//           debugLog: true, // optional, default
+//         },
+//       },
+//     });
+
+//     return config;
+//   },
+// };
 
 const nextConfig = {
   webpack: (config, { dev, isServer, webpack, nextRuntime }) => {
@@ -10,10 +119,6 @@ const nextConfig = {
       use: [
         {
           loader: "pgml-nextjs-node-loader",
-          options: {
-            flags: os.constants.dlopen.RTLD_NOW,
-            outputPath: config.output.path,
-          },
         },
       ],
     });
